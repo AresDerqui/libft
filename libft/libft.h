ff0a87277995ee924abbf5715d5c38ce2eee3ddc
AresDerqui <aresderqui@gmail.com>
2026-05-26 20:14:06 +0200

    Add files via upload

---
+++ libft/libft.h
@@ -0,0 +1,16 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+/* estructura para parte de listas enlazadas*/
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;i
+}					t_list;
+
+/* prototipos apareceran aqui*/
+
+#endif
